b0704e15c535871182851ef46c9f09556a106fa8
Mitko <mitkoo@gmail.com>
2018-06-14 13:56:46 +1000

    Firehose delivery stream details for transformation function, s3 backup bucket, error logging to cloudwatch

--- lib/awspec/matcher/have_splunk_destination.rb
+++ lib/awspec/matcher/have_splunk_destination.rb
@@ -6,8 +6,11 @@ RSpec::Matchers.define :have_splunk_destination do
     match = match && delivery_stream.splunk_destination.hec_token == @hec_token if @hec_token
     match = match && delivery_stream.splunk_destination.hec_acknowledgment_timeout_in_seconds == @hec_acknowledgment_timeout_in_seconds if @hec_acknowledgment_timeout_in_seconds
     match = match && delivery_stream.splunk_destination.s3_backup_mode == @s3_backup_mode if @s3_backup_mode
+
     match = match && delivery_stream.splunk_destination.retry_options.to_hash == @retry_options if @retry_options
     match = match && delivery_stream.splunk_destination.cloud_watch_logging_options.to_hash == @cloud_watch_logging_options if @cloud_watch_logging_options
+    match = match && delivery_stream.splunk_destination.s3_destination_description.to_hash == @s3_destination_description if @s3_destination_description
+    match = match && delivery_stream.splunk_destination.processing_configuration.to_hash == @processing_configuration if @processing_configuration
     match
   end
 
@@ -43,17 +46,14 @@ RSpec::Matchers.define :have_splunk_destination do
     @cloud_watch_logging_options = cloud_watch_logging_options
   end
 
+  chain :s3_destination_description do |s3_destination_description|
+    @s3_destination_description = s3_destination_description
+  end
+
+  chain :processing_configuration do |processing_configuration|
+    @processing_configuration = processing_configuration
+  end
+
   # diffable
   # TODO: Override error message, because this is not diffable
 end
-
-# s3_destination_description.role_arn #=> String
-# s3_destination_description.bucket_arn #=> String
-# s3_destination_description.prefix #=> String
-# s3_destination_description.buffering_hints.size_in_m_bs #=> Integer
-# s3_destination_description.buffering_hints.interval_in_seconds #=> Integer
-# s3_destination_description.compression_format #=> String, one of "UNCOMPRESSED", "GZIP", "ZIP", "Snappy"
-
-# s3_destination_description.encryption_configuration.no_encryption_config #=> String, one of "NoEncryption"
-# s3_destination_description.encryption_configuration.kms_encryption_config.awskms_key_arn #=> String
-
